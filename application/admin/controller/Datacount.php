80b22063a98b3a0afcd7ddc595fe687ab7f1e56c
15711270182 <2567588127@qq.com>
2021-09-01 10:13:39 +0800

    1

--- application/admin/controller/Datacount.php
+++ application/admin/controller/Datacount.php
@@ -241,7 +241,7 @@ class Datacount extends Controller
         $list = $this->getInfoList($start, $end);
         foreach ($list as $key => $value){
             $list[$key]['sex'] = $value['sex'] == 1 ? '男':'女';
-            $list[$key]['create_at'] = date('Y-m-d H:i:m', $value['create_at']);
+            $list[$key]['create_at'] = date('Y-m-d H:i:m', $value['info_check_time']);
         }
         $this->assign('list', $list);
         return $this->fetch();
