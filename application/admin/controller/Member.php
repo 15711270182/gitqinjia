d2f49130947b3a7cc6ac3ec1f615bfaa0a7a0d1b
15711270182 <2567588127@qq.com>
2021-09-02 17:50:26 +0800

    1

--- application/admin/controller/Member.php
+++ application/admin/controller/Member.php
@@ -75,6 +75,11 @@ class Member extends Controller
         $province = input('province','');
         $residence = input('residence','');
 
+        $hometown = input('hometown','');
+        $filename = env('root_path') . 'public/static/plugs/jquery/area/area.json';
+        $this->provinces  = array_column(json_decode(file_get_contents($filename), true), 'name');
+        $this->hometown = $hometown;
+
         $this->url = urldecode($_SERVER['REQUEST_URI']);
         $this->url = str_replace("&search_tel=asc", '', $this->url);
         $this->url = str_replace("&search_tel=desc", '', $this->url);
@@ -107,7 +112,7 @@ class Member extends Controller
         if(!empty($province) && !empty($residence)){
             if($residence != '请选择'){
                 $residence =  str_replace(['市'],'',$residence);
-                $where .= " and residence like '%{$residence}%'";
+                $where .= " and c.residence like '%{$residence}%'";
             }
         }
         if(!empty($bid)){
@@ -156,7 +161,7 @@ class Member extends Controller
 
         $field = "u.id,u.nickname,u.headimgurl,u.is_vip,u.add_time,u.count,u.status,c.expect_education,c.min_age,c.min_height,c.id as cid,c.phone,
         c.sex as xingbie,c.is_ban,c.year,c.province,c.residence,c.team_status,c.weight_score,c.remarks_text,(select count(*) from tel_collection t where t.bid = c.uid and t.status=1) as look_tel";
-        $equal = 'u.id#id,u.nickname#nickname,u.is_vip#is_vip,c.phone#phone,c.sex#sex,u.status#status,c.education#education,c.year#year,c.team_status#team_status,c.cart#cart,c.house#house';
+        $equal = 'u.id#id,u.nickname#nickname,u.is_vip#is_vip,c.phone#phone,c.sex#sex,u.status#status,c.education#education,c.year#year,c.team_status#team_status,c.cart#cart,c.house#house,c.hometown#hometown';
         $this->_query($this->table)
                 ->alias('u')
                 ->field($field)
