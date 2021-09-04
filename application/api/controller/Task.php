1e901bdff1967a6aef7f4f7464f80b5a89eefc88
15711270182 <2567588127@qq.com>
2021-09-04 17:48:33 +0800

    1

--- application/api/controller/Task.php
+++ application/api/controller/Task.php
@@ -5,6 +5,7 @@ namespace app\api\controller;
 
 use think\Db;
 use app\api\controller\Base;
+use app\api\service\Send as SendService;
 /**
  * Class Task
  */
@@ -56,90 +57,15 @@ class Task extends Base
     }
 
    public function send(){
-       /*****************
-         * 非加密请求 示例代码
-         ******************/
-        //appid参数 appkey参数在     短信-创建/管理AppID中获取
-        //手机号支持单个
-        //短信内容：签名+正文    详细规则查看短信-模板管理
-//        $appid = '67062';                                                               //appid参数
-//        $appkey = 'ecdfa8e0597260657535c24bb18b1cb9';                                   //appkey参数
-//        $to = '18551821638';                                                            //收信人 手机号码
-//        $project_id = 'pjjUb4';                                                           //模板ID
-//        $vars = json_encode([                                                    //模板对应变量
-//           'realname' => '张',
-//           'url' => 'http://www.baidu.com'
-//        ]);
-//
-//        $post_data = array(
-//            "appid"     => $appid,
-//            "signature" => $appkey,
-//            "to"        => $to,
-//            "project"   => $project_id,
-//            "vars"      => $vars
-//        );
-//        $ch = curl_init();
-//        curl_setopt_array($ch, array(
-//            CURLOPT_URL            => 'https://api.mysubmail.com/message/xsend.json',
-//            CURLOPT_RETURNTRANSFER => 1,
-//            CURLOPT_POST           => 1,
-//            CURLOPT_POSTFIELDS     => $post_data
-//        ));
-//        $output = curl_exec($ch);
-//        curl_close($ch);
-//        echo $output;die;
-
-
-        $appid = '67062';                                                               //appid参数
-        $appkey = 'ecdfa8e0597260657535c24bb18b1cb9';                                   //appkey参数
         $to = '15711270182';                                                            //收信人 手机号码
         $project_id = 'pjjUb4';                                                           //模板ID
         $vars = json_encode([                                                  //模板对应变量
             'realname' => '张',
-            'url' => 'https://testqin.njzec.com/h5/web/send'
+            'url' => 'v1kj.cn'
         ]);
-        //通过接口获取时间戳
-        $ch = curl_init();
-        curl_setopt_array($ch, array(
-            CURLOPT_URL            => 'https://api.mysubmail.com/service/timestamp.json',
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_POST           => 0
-        ));
-        $output = curl_exec($ch);
-        curl_close($ch);
-        $output = json_decode($output, true);
-        $timestamp = $output['timestamp'];
-        $post_data = [
-            "appid"        => $appid,
-            "to"           => $to,
-            "project"      => $project_id,
-            "timestamp"    => $timestamp,
-            "sign_type"    => 'md5',
-            "sign_version" => 2,
-            "vars"         => $vars ,
-        ];
-        //整理生成签名所需参数
-        $temp = $post_data;
-        unset($temp['vars']);
-        ksort($temp);
-        reset($temp);
-        $tempStr = "";
-        foreach ($temp as $key => $value) {
-            $tempStr .= $key . "=" . $value . "&";
-        }
-        $tempStr = substr($tempStr, 0, -1);
-        //生成签名
-        $post_data['signature'] = md5($appid . $appkey . $tempStr . $appid . $appkey);
-        $ch = curl_init();
-        curl_setopt_array($ch, array(
-            CURLOPT_URL            => 'https://api.mysubmail.com/message/xsend.json',
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_POST           => 1,
-            CURLOPT_POSTFIELDS     => $post_data
-        ));
-        $output = curl_exec($ch);
-        curl_close($ch);
-        echo json_encode($output);
+        $send = new SendService();
+        $res = $send->sendMsg($to,$project_id,$vars);
+        var_dump($res);
    }
 
 }
---
+++ application/api/service/Send.php
@@ -0,0 +1,109 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | ThinkAdmin
+// +----------------------------------------------------------------------
+// | 版权所有 2014~2019 广州楚才信息科技有限公司 [ http://www.cuci.cc ]
+// +----------------------------------------------------------------------
+// | 官方网站: http://demo.thinkadmin.top
+// +----------------------------------------------------------------------
+// | 开源协议 ( https://mit-license.org )
+// +----------------------------------------------------------------------
+// | gitee 代码仓库：https://gitee.com/zoujingli/ThinkAdmin
+// | github 代码仓库：https://github.com/zoujingli/ThinkAdmin
+// +----------------------------------------------------------------------
+
+namespace app\api\service;
+
+use think\Db;
+use think\Request;
+
+/**
+ * Class Send
+ * @package app\store\controller\api
+ */
+class Send
+{
+    /**
+     * 获取短信配置
+     * @return array
+     */
+    private function config()
+    {
+        return config('send');
+    }
+
+    /**
+     * 生成签名
+     */
+    public function signature($post_data)
+    {
+        $appid = config('send.appid');
+        $appkey = config('send.appsecret');
+        //整理生成签名所需参数
+        $temp = $post_data;
+        unset($temp['vars']);
+        ksort($temp);
+        reset($temp);
+        $tempStr = "";
+        foreach ($temp as $key => $value) {
+            $tempStr .= $key . "=" . $value . "&";
+        }
+        $tempStr = substr($tempStr, 0, -1);
+        //生成签名
+        $post_data['signature'] = md5($appid . $appkey . $tempStr . $appid . $appkey);
+        return $post_data;
+    }
+     /**
+     * 生成时间戳
+     */
+    public function getTimestamp(){
+        $url = config('send.main_url')."service/timestamp.json";
+        //通过接口获取时间戳
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL            => $url,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_POST           => 0
+        ));
+        $output = curl_exec($ch);
+        curl_close($ch);
+        $output = json_decode($output, true);
+        $timestamp = $output['timestamp'];
+        return $timestamp;
+    }
+
+    /**
+     * @Notes:发送短信
+     * @Interface sendMsg
+     * @author: zy
+     * @Time: ${DATE}   ${TIME}
+     */
+    public function sendMsg($to,$project_id,$vars){
+        $appid = config('send.appid');
+        $appkey = config('send.appsecret');
+        $m_url = config('send.main_url')."message/xsend.json";
+        //通过接口获取时间戳
+        $timestamp = self::getTimestamp();
+        $post_data = [
+            "appid"        => $appid,
+            "to"           => $to,
+            "project"      => $project_id,
+            "timestamp"    => $timestamp,
+            "sign_type"    => 'md5',
+            "sign_version" => 2,
+            "vars"         => $vars ,
+        ];
+        $post_data = self::signature($post_data,$appid,$appkey);
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL            => $m_url,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_POST           => 1,
+            CURLOPT_POSTFIELDS     => $post_data
+        ));
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+}
